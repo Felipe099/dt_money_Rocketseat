a495669a8a07f2082b694736b3c1f974f1baa1f9
Felipe099 <lipepagodart99@gmail.com>
2022-04-06 18:55:11 -0300

    falta colocar novo render react 18

--- src/components/TransctionsTable/index.tsx
+++ src/components/TransctionsTable/index.tsx
@@ -1,33 +1,40 @@
-import { Container } from "./style";
+import { useEffect } from 'react';
+import { api } from '../../services/api';
+import { Container } from './style';
 
 export function TransctionsTable() {
-  return (
-  <Container>
-    <table>
-      <thead>
-        <tr>
-          <th>Título</th>
-          <th>Valor</th>
-          <th>Categoria</th>
-          <th>Data</th>
-        </tr>
-      </thead>
+    useEffect(() => {
+        api.get('transactions')
+        .then((response) => console.log(response.data));
+    }, []);
 
-      <tbody>
-        <tr>
-          <td>Desenvolvimento de website</td>
-          <td className="deposit">R$ 12000</td>
-          <td>Desenvolvimento</td>
-          <td>05/04/2022</td>
-        </tr>
-        <tr>
-          <td>Aluguel</td>
-          <td className="withdraw">- R$ 1000</td>
-          <td>Casa</td>
-          <td>01/04/2022</td>
-        </tr>
-      </tbody>
-    </table>
-  </Container>
-)
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$ 12000</td>
+                        <td>Desenvolvimento</td>
+                        <td>05/04/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$ 1000</td>
+                        <td>Casa</td>
+                        <td>01/04/2022</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,36 @@
+//import { createRoot } from 'react-dom'
+import { createServer } from 'miragejs';
+import { App } from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';
+
+createServer({
+    routes() {
+        this.namespace = 'api';
+
+        this.get('/transactions', () => {
+            return [
+                {
+                    id: '1',
+                    title: 'transactions 1',
+                    amount: 400,
+                    type: 'deposit',
+                    category: 'food',
+                    createAt: new Date(),
+                },
+            ];
+        });
+    },
+});
+
+//const container = document.getElementById('app');
+//const root = createRoot(container);
+//root.render(<App />); 
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
